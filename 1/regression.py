6c069cfd1a294e70d53d4eb292af29e6ec700206
Tomas Santana <tomas.30604530@uru.edu>
2025-02-08 22:40:23 -0600

    fix broken import

--- 1/regression.py
+++ 1/regression.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import root_mean_squared_error
-from lib.LinReg import LinearRegression as MyLinearRegression
+from lib.LinearRegression import LinearRegression as MyLinearRegression
 
 df = pd.read_csv("datasets/pitch_data_2024_cleaned.csv")
 
