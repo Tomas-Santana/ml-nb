ac30b3aca62b606728a3da701d2c3ea4b219ecb4
Tomas Santana <tomas.30604530@uru.edu>
2025-04-20 23:30:09 -0500

    Notebook check 5. Random forest and SVC

--- src/5/max_depth_cv.py
+++ src/5/max_depth_cv.py
@@ -8,7 +8,7 @@ df = pd.read_csv("./datasets/obesity_clean.csv")
 
 y = df.iloc[:, -1].to_numpy()
 X = df.iloc[:, :-1].to_numpy()
-X = (X - X.mean(axis=0)) / X.std(axis=0)
+# X = (X - X.mean(axis=0)) / X.std(axis=0)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
@@ -19,18 +19,30 @@ grid_search.fit(X_train, y_train)
 clf = grid_search.best_estimator_
 
 score_df = pd.DataFrame(grid_search.cv_results_)
-print(score_df[["param_max_depth", "mean_test_score"]])
+print(score_df[["param_max_depth", "mean_test_score", "rank_test_score"]])
 
+fig, ax = plt.subplots(figsize=(10, 10))
 
-disp = ConfusionMatrixDisplay.from_estimator(clf, X_test, y_test)
+disp = ConfusionMatrixDisplay.from_estimator(clf, X_test, y_test, ax=ax)
 disp.ax_.set_title("Confusion matrix")
-plt.show()
+
+
+for label in disp.ax_.get_xticklabels():
+    label.set_fontsize(8)
+    label.set_rotation(45)
+for label in disp.ax_.get_yticklabels():
+    label.set_fontsize(8)
+
+
+
+plt.savefig("./src/5/images/decision_tree_confusion_matrix.png", dpi=100)
+
 
 # plot max_depth == 3 tree
+class_names = sorted(df.iloc[:, -1].unique())
 max_depth_3_clf = DecisionTreeClassifier(max_depth=3)
 max_depth_3_clf.fit(X_train, y_train)
 plt.figure(figsize=(20, 20))
-plot_tree(max_depth_3_clf, filled=True, fontsize=10, feature_names=df.columns[:-1], rounded=True)
+plot_tree(max_depth_3_clf, filled=True, fontsize=10, feature_names=df.columns[:-1], class_names=class_names, rounded=True)
 plt.title("Decision Tree with max_depth=3")
-plt.savefig("./src/5/images/max_depth_3_tree.png", dpi=100)
-
+plt.savefig("./src/5/images/max_depth_3_tree.png")
--- src/5/svc_cv.py
+++ src/5/svc_cv.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
@@ -13,30 +12,19 @@ X = (X - X.mean(axis=0)) / X.std(axis=0)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-# SVC
-# svc_clf = SVC()
-# param_grid = {"C": np.logspace(-2, 2, 5), "gamma": np.logspace(-2, 2, 5)}
-# grid_search = GridSearchCV(svc_clf, param_grid, verbose=5)
-# grid_search.fit(X_train, y_train)
-# svc_clf = grid_search.best_estimator_
 
-# score_df = pd.DataFrame(grid_search.cv_results_)
-# print("SVC Results:")
-# print(score_df[[ "param_C", "param_gamma", "mean_test_score", "rank_test_score"]])
-# disp = ConfusionMatrixDisplay.from_estimator(svc_clf, X_test, y_test)
-# disp.ax_.set_title("SVC Confusion matrix")
-# plt.show()
-
-# Random Forest
-rf_clf = RandomForestClassifier()
-param_grid = {"n_estimators": [10, 50, 100], "max_depth": range(3, 15)}
-grid_search = GridSearchCV(rf_clf, param_grid, verbose=5)
+svc_clf = SVC()
+param_grid = {"C": np.logspace(-2, 2, 5), "gamma": np.logspace(-2, 2, 5)}
+grid_search = GridSearchCV(svc_clf, param_grid, verbose=5, n_jobs=-1)
 grid_search.fit(X_train, y_train)
-rf_clf = grid_search.best_estimator_
+svc_clf = grid_search.best_estimator_
+
 score_df = pd.DataFrame(grid_search.cv_results_)
-print("Random Forest Results:")
-print(score_df[["param_n_estimators", "param_max_depth", "mean_test_score", "rank_test_score"]])
-disp = ConfusionMatrixDisplay.from_estimator(rf_clf, X_test, y_test)
-disp.ax_.set_title("Random Forest Confusion matrix")
-plt.show()
+print("SVC Results:")
+print(score_df[[ "param_C", "param_gamma", "mean_test_score", "rank_test_score"]])
+disp = ConfusionMatrixDisplay.from_estimator(svc_clf, X_test, y_test)
+disp.ax_.set_title("SVC Confusion matrix")
+plt.savefig("src/5/images/svc_cm.png")
+
+
 
